b0f54f7d2f58af35910a1312afb95333d31e7c1c
Peter Popov <p.dim.popov@gmail.com>
2022-02-05 14:33:42 +0200

    refactor(SimpleLinkCard): asjust semantics

--- src/components/SimpleLinkCard.tsx
+++ src/components/SimpleLinkCard.tsx
@@ -14,27 +14,31 @@ const SimpleLinkCard: React.FC<ISimpleLinkCardProps> = ({
     description,
     footer,
 }) => (
-    <Link href={href} passHref>
-        <a>
-            <div className="w-full max-w-sm lg:flex lg:max-w-full">
-                <div className="flex flex-col justify-between rounded-b border-r border-b border-l border-gray-400 bg-white p-4 leading-normal lg:rounded-b-none lg:rounded-r lg:border-l-0 lg:border-t lg:border-gray-400">
-                    <div className="mb-8">
-                        <div className="mb-2 text-xl font-bold text-gray-900">
-                            {title}
+    <li className="list-none">
+        <Link href={href} passHref>
+            <a>
+                <div className="w-full max-w-sm lg:flex lg:max-w-full">
+                    <div className="flex flex-col justify-between rounded-b border-r border-b border-l border-gray-400 bg-white p-4 leading-normal lg:rounded-b-none lg:rounded-r lg:border-l-0 lg:border-t lg:border-gray-400">
+                        <div className="mb-8">
+                            <section className="mb-2 text-xl font-bold text-gray-900">
+                                {title}
+                            </section>
+                            <div className="text-base text-gray-700">
+                                {description}
+                            </div>
                         </div>
-                        <p className="text-base text-gray-700">{description}</p>
-                    </div>
-                    <div className="flex items-center">
-                        <div className="text-sm">
-                            <p className="leading-none text-gray-900">
-                                {footer}
-                            </p>
+                        <div className="flex items-center">
+                            <div className="text-sm">
+                                <section className="leading-none text-gray-900">
+                                    {footer}
+                                </section>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </a>
-    </Link>
+            </a>
+        </Link>
+    </li>
 );
 
 export default SimpleLinkCard;
